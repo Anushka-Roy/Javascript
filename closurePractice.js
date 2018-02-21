34b2806780153f23a081f1a1547f2530ea6f4f1b
Anushka-Roy <aroy108@gmail.com>
2018-02-21 10:55:38 +0530

    You can create a closure to keep the value passed to the function createBase even after the inner function is returned. The inner function that is being returned is created within an outer function, making it a closure, and it has access to the variables within the outer function, in this case the variable baseNumber.

---
+++ closurePractice.js
@@ -0,0 +1,16 @@
+function createBase(baseNumber) {
+  return function(N) {
+    // we are referencing baseNumber here even though it was declared
+    // outside of this function. Closures allow us to do this in JavaScript
+    return baseNumber + N;
+  }
+}
+
+var addSix = createBase(6);
+console.log(addSix(10));
+console.log(addSix(21));
+
+/**output
+16
+27
+**/
