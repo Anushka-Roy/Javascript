7278acadfe2f39f2aa2659c662d5aae73d4eefdc
Anushka-Roy <aroy108@gmail.com>
2018-02-21 10:34:13 +0530

    Create setTimeES6.js
    using the ES6 let syntax, it creates a new binding every single time the function is called.

---
+++ setTimeES6.js
@@ -0,0 +1,15 @@
+const arr = [10, 12, 15, 21];
+for (let i = 0; i < arr.length; i++) {
+  // using the ES6 let syntax, it creates a new binding
+  // every single time the function is called
+  // read more here: http://exploringjs.com/es6/ch_variables.html#sec_let-const-loop-heads
+  setTimeout(function() {
+    console.log('The index of this number is: ' + i);
+  }, 3000);
+}
+/**output
+"The index of this number is: 0"
+"The index of this number is: 1"
+"The index of this number is: 2"
+"The index of this number is: 3"
+**/
